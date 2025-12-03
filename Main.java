1948101ca76b20fb50bffd5960345f036168e74c
Erka888 <musaevaerkaim@gmail.com>
2025-12-03 08:31:17 +0600

    guessTheNumber

--- Main.java
+++ Main.java
@@ -1,55 +1,108 @@
-import java.util.Random;
-import java.util.Scanner;
+package org.example;
+public class Pet {
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+    String name;
+    int hunger;
+    int energy;
+    int mood;
 
-        final int N = 100;
+    public Pet(String petName, String breed) {
+        name = petName;
 
-        while (true) {
-            int secret = random.nextInt(N) + 1;
-            int attempts = 0;
-
-            System.out.println("Игра 'Угадай число'!");
-            System.out.println("Я загадал число от 1 до " + N);
-
-            while (true) {
-                System.out.print("Введите число: ");
-
-                if (!scanner.hasNextInt()) {
-                    System.out.println("Ошибка: нужно ввести число!");
-                    scanner.next();
-                    continue;
-                }
-
-                int guess = scanner.nextInt();
-                attempts++;
-
-                if (guess < 1 || guess > N) {
-                    System.out.println("Число вне диапазона!");
-                    continue;
-                }
-
-                if (guess == secret) {
-                    System.out.println("Поздравляю! Вы угадали за " + attempts + " попыток!");
-                    break;
-                } else if (guess < secret) {
-                    System.out.println("Больше!");
-                } else {
-                    System.out.println("Меньше!");
-                }
-            }
-
-            System.out.print("Сыграть снова? (y/n): ");
-            String again = scanner.next();
-            if (!again.equalsIgnoreCase("y")) {
-                System.out.println("Спасибо за игру!");
-                break;
-            }
+        if (breed.equals("1")) { //кошка
+            hunger = 30;
+            energy = 80;
+            mood = 60;
+        } else if (breed.equals("2")) { //собака
+            hunger = 40;
+            energy = 90;
+            mood = 50;
+        } else if (breed.equals("3")) { //хомяк
+            hunger = 20;
+            energy = 70;
+            mood = 80;
+        } else if (breed.equals("4")) { //кролик
+            hunger = 25;
+            energy = 75;
+            mood = 65;
+        } else {   //по умолчанию
+            hunger = 50;
+            energy = 50;
+            mood = 50;
         }
+    }
+
+    // Действия питомца
+    public void feed() {
+        hunger -= 20;
+        mood += 5;
+        normalize();
+        System.out.println(name + " поел! ");
+    }
+    public void play() {
+        mood += 20;
+        hunger += 10;
+        energy -= 15;
+        normalize();
+        System.out.println(name + " играет! ");
+    }
+    public void sleep() {
+        energy += 25;
+        hunger += 10;
+        normalize();
+        System.out.println(name + " спит! ");
+    }
+    public void doNothing() {
+        hunger += 5;
+        mood -= 5;
+        normalize();
+        System.out.println(name + " ничего не делает ");
+    }
+    // Ухудшение состояния со временем
+    public void tick() {
+        hunger += 3;
+        mood -= 2;
+        energy -= 1;
+        normalize();
+    }
+    // Ограничение значений от 0 до 100
+    void normalize() {
+        if (hunger < 0) hunger = 0;
+        if (hunger > 100) hunger = 100;
+
+        if (energy < 0) energy = 0;
+        if (energy > 100) energy = 100;
+
+        if (mood < 0) mood = 0;
+        if (mood > 100) mood = 100;
+    }
+
+    // Проверка, жив ли питомец
+    boolean isAlive() {
+        return hunger < 100 && energy > 0 && mood > 0;
+    }
+
+    String getFace() {
+        if (!isAlive()) {
+            if (hunger >= 100) return "Питомец умер от голода";
+            if (energy <= 0) return "Питомец умер от усталости";
+            if (mood <= 0) return "Питомец умер от плохого настроения";
+            return "(x_x)";
+        }
+
+        if (energy < 30) return "(−﹏−)";
+        if (mood < 30) return "(╥_╥)";
+        if (hunger > 70) return "(ᵕ︵ᵕ)";
+        if (mood > 70) return "(=^ ◡ ^=)";
+        return "❤\uFE0F";
+    }
+
+    void printStatus() {
+        System.out.println("-- Статус питомца --");
+        System.out.println("Имя: " + name + " " + getFace());
+        System.out.println("Голод: " + hunger);
+        System.out.println("Энергия: " + energy);
+        System.out.println("Настроение: " + mood);
+        System.out.println("======================");
     }
 }
