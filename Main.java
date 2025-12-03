be4ca440f69b66442510bfefd6f3ebc449d0e584
Erka888 <musaevaerkaim@gmail.com>
2025-12-03 09:09:18 +0600

    Guess

--- Main.java
+++ Main.java
@@ -1,108 +1,76 @@
 package org.example;
-public class Pet {
 
-    String name;
-    int hunger;
-    int energy;
-    int mood;
+import java.util.Random;
+import java.util.Scanner;
 
-    public Pet(String petName, String breed) {
-        name = petName;
 
-        if (breed.equals("1")) { //кошка
-            hunger = 30;
-            energy = 80;
-            mood = 60;
-        } else if (breed.equals("2")) { //собака
-            hunger = 40;
-            energy = 90;
-            mood = 50;
-        } else if (breed.equals("3")) { //хомяк
-            hunger = 20;
-            energy = 70;
-            mood = 80;
-        } else if (breed.equals("4")) { //кролик
-            hunger = 25;
-            energy = 75;
-            mood = 65;
-        } else {   //по умолчанию
-            hunger = 50;
-            energy = 50;
-            mood = 50;
+public class GuessTheNumberrr {
+    public static void main(String[] args) {
+        startGame();
+    }
+
+    public static void startGame() {
+        Scanner sc = new Scanner(System.in);
+        Random rnd = new Random();
+        System.out.println("Welcome to the Guess the Number game.");
+
+        while (true) {
+            int maxNumber = chooseDifficulty(sc);
+            int secretNumber = rnd.nextInt(maxNumber) + 1; // number from 1-100
+            int attempts = 0;
+
+            System.out.println("I have chosen a number between 1 and 100. Guess.");
+
+            while (true) {
+                System.out.print("Enter  your guess: ");
+                int guess = readInt(sc);
+                attempts++;
+
+                if (guess < secretNumber) {
+                    System.out.println("small");
+                } else if (guess > secretNumber) {
+                    System.out.println("big");
+                } else {
+                    System.out.println("You guessed it");
+                    System.out.println("Attempts used: " + attempts);
+                    break;
+                }
+            }
+            System.out.println("Do you want to play again? (yes/no): ");
+            String answer = sc.next().toLowerCase();
+            sc.nextLine(); //clear input buffer
+
+            if (!answer.equals("y")) {
+                System.out.println("Thanks for playing. Goodbye ");
+                break;
+            }
         }
     }
+    public static  int chooseDifficulty(Scanner sc ) {
+        System.out.println("Choose difficulty level: ");
+        System.out.println("1. Easy (1-50)");
+        System.out.println("2. Medium (1-100)");
+        System.out.println("3. Hard (1-500)");
 
-    // Действия питомца
-    public void feed() {
-        hunger -= 20;
-        mood += 5;
-        normalize();
-        System.out.println(name + " поел! ");
-    }
-    public void play() {
-        mood += 20;
-        hunger += 10;
-        energy -= 15;
-        normalize();
-        System.out.println(name + " играет! ");
-    }
-    public void sleep() {
-        energy += 25;
-        hunger += 10;
-        normalize();
-        System.out.println(name + " спит! ");
-    }
-    public void doNothing() {
-        hunger += 5;
-        mood -= 5;
-        normalize();
-        System.out.println(name + " ничего не делает ");
-    }
-    // Ухудшение состояния со временем
-    public void tick() {
-        hunger += 3;
-        mood -= 2;
-        energy -= 1;
-        normalize();
-    }
-    // Ограничение значений от 0 до 100
-    void normalize() {
-        if (hunger < 0) hunger = 0;
-        if (hunger > 100) hunger = 100;
+        while (true) {
+            int choice = readInt(sc);
+            switch (choice) {
+                case 1: return 50;
+                case 2: return 100;
+                case 3: return 500;
+                default:System.out.print("Invalid choice, try again: ");
 
-        if (energy < 0) energy = 0;
-        if (energy > 100) energy = 100;
-
-        if (mood < 0) mood = 0;
-        if (mood > 100) mood = 100;
-    }
-
-    // Проверка, жив ли питомец
-    boolean isAlive() {
-        return hunger < 100 && energy > 0 && mood > 0;
-    }
-
-    String getFace() {
-        if (!isAlive()) {
-            if (hunger >= 100) return "Питомец умер от голода";
-            if (energy <= 0) return "Питомец умер от усталости";
-            if (mood <= 0) return "Питомец умер от плохого настроения";
-            return "(x_x)";
+            }
         }
-
-        if (energy < 30) return "(−﹏−)";
-        if (mood < 30) return "(╥_╥)";
-        if (hunger > 70) return "(ᵕ︵ᵕ)";
-        if (mood > 70) return "(=^ ◡ ^=)";
-        return "❤\uFE0F";
     }
+    public static int readInt(Scanner sc) {
+        while (true) {
+            String input = sc.nextLine();
 
-    void printStatus() {
-        System.out.println("-- Статус питомца --");
-        System.out.println("Имя: " + name + " " + getFace());
-        System.out.println("Голод: " + hunger);
-        System.out.println("Энергия: " + energy);
-        System.out.println("Настроение: " + mood);
-        System.out.println("======================");
+            try {
+                return Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number: ");
+            }
+        }
     }
-}
